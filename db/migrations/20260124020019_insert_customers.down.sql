2bec65449a0f18b90488684383ca108309629544
Ahmad Aziz Bin Agus <aziz007ahmad@gmail.com>
2026-01-24 09:08:17 +0700

    second commit

---
+++ db/migrations/20260124020019_insert_customers.down.sql
@@ -0,0 +1,3 @@
+delete from customers where cst_id is not null;
+
+delete from customers where cst_id is null;
---
+++ db/migrations/20260124020019_insert_customers.up.sql
@@ -0,0 +1,63 @@
+-- Data Customers (40 pemain bola)
+INSERT INTO customers (cst_id, nationality_id, cst_name, cst_dob, cst_phoneNum, cst_email) VALUES
+-- Pemain Indonesia
+(1, 1, 'Bambang Pamungkas', '1980-06-10', '081122334455', 'bambang@example.com'),
+(2, 1, 'Evan Dimas', '1995-03-13', '081233445566', 'evan@example.com'),
+(3, 1, 'Septian David', '1996-09-02', '081344556677', 'septian@example.com'),
+(4, 1, 'Ricky Kambuaya', '1996-11-05', '081455667788', 'ricky@example.com'),
+
+-- Pemain Brazil
+(5, 2, 'Neymar Jr', '1992-02-05', '551199999999', 'neymar@example.com'),
+(6, 2, 'Vinicius Jr', '2000-07-12', '551188888888', 'vinicius@example.com'),
+(7, 2, 'Alisson Becker', '1992-10-02', '551177777777', 'alisson@example.com'),
+(8, 2, 'Casemiro', '1992-02-23', '551166666666', 'casemiro@example.com'),
+(9, 2, 'Richarlison', '1997-05-10', '551155555555', 'richarlison@example.com'),
+(10, 2, 'Gabriel Jesus', '1997-04-03', '551144444444', 'gabriel@example.com'),
+
+-- Pemain Argentina
+(11, 3, 'Lionel Messi', '1987-06-24', '541199999999', 'messi@example.com'),
+(12, 3, 'Angel Di Maria', '1988-02-14', '541188888888', 'dimaria@example.com'),
+(13, 3, 'Lautaro Martinez', '1997-08-22', '541177777777', 'lautaro@example.com'),
+(14, 3, 'Julian Alvarez', '2000-01-31', '541166666666', 'alvarez@example.com'),
+(15, 3, 'Emiliano Martinez', '1992-09-02', '541155555555', 'emartinez@example.com'),
+
+-- Pemain Portugal
+(16, 4, 'Cristiano Ronaldo', '1985-02-05', '351999999999', 'ronaldo@example.com'),
+(17, 4, 'Bernardo Silva', '1994-08-10', '351888888888', 'bernardo@example.com'),
+(18, 4, 'Joao Felix', '1999-11-10', '351777777777', 'felix@example.com'),
+(19, 4, 'Ruben Dias', '1997-05-14', '351666666666', 'ruben@example.com'),
+(20, 4, 'Bruno Fernandes', '1994-09-08', '351555555555', 'bruno@example.com'),
+
+-- Pemain Spanyol
+(21, 5, 'Pedri', '2002-11-25', '34999999999', 'pedri@example.com'),
+(22, 5, 'Gavi', '2004-08-05', '34888888888', 'gavi@example.com'),
+(23, 5, 'Alvaro Morata', '1992-10-23', '34777777777', 'morata@example.com'),
+(24, 5, 'Dani Olmo', '1998-05-07', '34666666666', 'olmo@example.com'),
+
+-- Pemain Inggris
+(25, 6, 'Harry Kane', '1993-07-28', '441999999999', 'kane@example.com'),
+(26, 6, 'Phil Foden', '2000-05-28', '441888888888', 'foden@example.com'),
+(27, 6, 'Jude Bellingham', '2003-06-29', '441777777777', 'bellingham@example.com'),
+(28, 6, 'Bukayo Saka', '2001-09-05', '441666666666', 'saka@example.com'),
+
+-- Pemain Perancis
+(29, 7, 'Kylian Mbappe', '1998-12-20', '331999999999', 'mbappe@example.com'),
+(30, 7, 'Antoine Griezmann', '1991-03-21', '331888888888', 'griezmann@example.com'),
+(31, 7, 'Eduardo Camavinga', '2002-11-10', '331777777777', 'camavinga@example.com'),
+(32, 7, 'Olivier Giroud', '1986-09-30', '331666666666', 'giroud@example.com'),
+
+-- Pemain Jerman
+(33, 8, 'Jamal Musiala', '2003-02-26', '491999999999', 'musiala@example.com'),
+(34, 8, 'Kai Havertz', '1999-06-11', '491888888888', 'havertz@example.com'),
+(35, 8, 'Joshua Kimmich', '1995-02-08', '491777777777', 'kimmich@example.com'),
+
+-- Pemain Italia
+(36, 9, 'Nicolo Barella', '1997-02-07', '390999999999', 'barella@example.com'),
+(37, 9, 'Federico Chiesa', '1997-10-25', '390888888888', 'chiesa@example.com'),
+
+-- Pemain Belanda
+(38, 10, 'Virgil van Dijk', '1991-07-08', '310999999999', 'vandijk@example.com'),
+(39, 10, 'Frenkie de Jong', '1997-05-12', '310888888888', 'dejong@example.com'),
+
+-- Pemain Korea Selatan
+(40, 16, 'Son Heung-min', '1992-07-08', '821099999999', 'son@example.com');
